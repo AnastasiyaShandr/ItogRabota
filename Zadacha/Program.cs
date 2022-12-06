766d503f831b57f1aa486490730006f88a10ceec
AnastasiyaPlotnikova <world771@yandex.ru>
2022-12-06 15:33:54 +0500

    Added array

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -1,3 +1,5 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
 
 string text = "hello, 2, world, :)";
+string[] array = text.Split(" ");
+string[] newArray = new string[array.Length];
